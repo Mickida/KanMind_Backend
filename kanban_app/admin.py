c799e7b18fb305dd66338e04f093645a60d322bc
Mickida <d.micklausch@me.com>
2026-08-26 11:08:58 +0200

    Add Board, Task, and Comment models

--- kanban_app/admin.py
+++ kanban_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from kanban_app.models import Board, Comment, Task
+
+admin.site.register(Board)
+admin.site.register(Task)
+admin.site.register(Comment)
