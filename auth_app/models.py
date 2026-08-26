2577e4d4b3b02956c06721a8ea8086448015d26c
Mickida <d.micklausch@me.com>
2026-08-26 11:03:54 +0200

    Add custom User model with email-based auth

--- auth_app/models.py
+++ auth_app/models.py
@@ -1,3 +1,41 @@
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, fullname, password=None):
+        if not email:
+            raise ValueError("Users must have an email address")
+        user = self.model(email=self.normalize_email(email), fullname=fullname)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, fullname, password=None):
+        user = self.create_user(email=email, fullname=fullname, password=password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    fullname = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["fullname"]
+
+    class Meta:
+        ordering = ["fullname"]
+
+    def __str__(self):
+        return self.fullname
--- core/settings.py
+++ core/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'kanban_app',
 ]
 
+AUTH_USER_MODEL = "auth_app.User"
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
